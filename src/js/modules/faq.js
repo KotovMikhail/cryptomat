0de74d7a17f334d179bbd289fb39eca89a6a23a9
Kotov Mikhail <mihail_kotov_86@mail.ru>
2019-08-20 18:02:00 +0300

    добавяет вёрстку десктоп

---
+++ src/js/modules/faq.js
@@ -0,0 +1,26 @@
+(function () {
+  document.addEventListener(`click`, (evt) => {
+    let btn;
+
+    if (evt.target.classList.contains(`faq__question`)) {
+      btn = evt.target;
+    } else if (evt.target.parentElement.classList.contains(`faq__question`)) {
+      btn = evt.target.parentElement;
+    }
+
+    if (btn) {
+      let block = btn.parentElement;
+      let openedBlocks = document.querySelectorAll(`.faq__block`);
+
+      if (block.classList.contains(`faq__block--opened`)) {
+
+        setTimeout(() => {
+          block.classList.remove(`faq__block--opened`);
+        }, 100);
+      } else {
+        block.classList.add(`faq__block--opened`);
+
+      }
+    }
+  }, true);
+})();
--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -5,7 +5,32 @@ $('.team__list').slick({
   dots: true,
   centerMode: false,
   focusOnSelect: true,
-  arrows: true,
-  centerPadding: '60px'
+  arrows: true
+});
 
+
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+
+    event.preventDefault();
+    let id = $(this).attr('href'),
+      pTop = ($(id).css("padding-top")),
+      top = ($(id).offset().top) - parseInt(pTop);
+    $('body, html').animate({
+      scrollTop: top
+    }, 1500);
+
+  });
+});
+
+
+
+document.addEventListener('scroll', function () {
+  const header = document.querySelector('.header');
+  if (pageYOffset > 0) {
+    header.style.transition = 'all ease 0.5s'
+    header.style.boxShadow = '0 0 0.3rem #ffffff';
+  } else {
+    header.style.boxShadow = 'none';
+  }
 });
