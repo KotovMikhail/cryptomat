3efc395680b78af32b2433e8cdba8764d4a14c2e
Kotov Mikhail <mihail_kotov_86@mail.ru>
2019-09-06 10:03:55 +0300

    добавляет анимацию

--- build/js/script.js
+++ build/js/script.js
@@ -5425,8 +5425,8 @@ $('.team__list').slick({
   dotsClass: 'slick-dots',
   centerMode: false,
   focusOnSelect: false,
-  draggable: false,
-  arrows: true,
+  draggable: true,
+  arrows: false,
   responsive: [{
       breakpoint: 1000,
       settings: {
@@ -5509,8 +5509,6 @@ langItem.addEventListener('click', (evt) => {
   subList.classList.toggle('lang__sub-list--open');
 });
 
-const mql = window.matchMedia(`(max-width: 999px)`);
-
 let swiper1 = new Swiper('.swiper-container-s1', {
   slidesPerView: 2,
   paginationClickable: true,
@@ -5542,6 +5540,10 @@ let swiper1 = new Swiper('.swiper-container-s1', {
   }
 
 });
+
+
+
+const mql = window.matchMedia(`(max-width: 999px)`);
 
 const widthInit = (width) => {
   if (width.matches) {
@@ -5610,22 +5612,245 @@ const widthInit = (width) => {
 widthInit(mql);
 mql.addListener(widthInit);
 
-// (function ($) {
-//   // Init ScrollMagic
-//   var ctrl = new ScrollMagic.Controller({
-//     globalSceneOptions: {
-//       triggerHook: 'onLeave'
-//     }
-//   });
+(function ($) {
+  // Init ScrollMagic
+  let ctrl = new ScrollMagic.Controller();
 
-//   $("#img").each(function () {
+  let scene = new ScrollMagic.Scene({
+      triggerElement: '.about__descr',
+      triggerHook: 0.9, // show, when scrolled 10% into view
+      duration: "120%", // hide 10% before exiting view (80% + 10% from bottom)
+      offset: 50
+    })
+    .setClassToggle('.about__descr', 'about__descr--show')
+    .addTo(ctrl);
 
-//     new ScrollMagic.Scene({
-//         triggerElement: this
-//       })
-//       .setPin(this)
-//       .addTo(ctrl);
+  let scene2 = new ScrollMagic.Scene({
+      triggerElement: '.paper__descr',
+      triggerHook: 0.9, // show, when scrolled 10% into view
+      duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+      offset: 50
+    })
+    .setClassToggle('.paper__descr', 'paper__descr--show')
+    .addTo(ctrl);
 
-//   });
+  let scene3 = new ScrollMagic.Scene({
+      triggerElement: '.roadmap__descr',
+      triggerHook: 0.9, // show, when scrolled 10% into view
+      duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+      offset: 50
+    })
+    .setClassToggle('.roadmap__descr', 'roadmap__descr--show')
+    .addTo(ctrl);
 
-// })(jQuery);
+  $('.title').each(function () {
+    let scene4 = new ScrollMagic.Scene({
+        triggerElement: this,
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 0
+      })
+      .setClassToggle(this, 'show')
+      .addTo(ctrl);
+  });
+
+
+
+
+})(jQuery);
+
+const flightPath = {
+  curviness: 0,
+  autoRotate: false,
+  values: [{
+      x: 50,
+      y: -10
+    },
+    {
+      x: 0,
+      y: -20
+    },
+    {
+      x: 0,
+      y: -50
+    },
+    {
+      x: 0,
+      y: -window.innerHeight
+    }
+  ]
+}
+
+
+const mobileWidth = window.matchMedia(`(max-width: 767px)`);
+
+const widthAnimateInit = (width) => {
+
+  if (!width.matches) {
+    const tween = new TimelineLite();
+
+    tween.to('.present__coin--first', 0.3, {
+        yPercent: -100,
+        xPercent: -50,
+        autoAlpha: 0,
+        ease: Power4.easeInOut
+      })
+      .to('.present__coin--second', 0.3, {
+        yPercent: -100,
+        xPercent: -20,
+        autoAlpha: 0,
+        ease: Power4.easeInOut
+      })
+      .to('.present__coin--third', 0.3, {
+        yPercent: -100,
+        xPercent: 50,
+        autoAlpha: 0,
+        ease: Power4.easeInOut
+
+      });
+
+    const controller = new ScrollMagic.Controller();
+
+    const scene = new ScrollMagic.Scene({
+        triggerElement: '.header',
+        duration: 800,
+        triggerHook: 0
+      })
+      .setTween(tween)
+      .setPin('.header')
+      .addTo(controller);
+
+
+    // IEO animate
+
+    const tween2 = new TimelineLite();
+
+    tween2.to('.ieo__img-coin--first', 0.3, {
+        yPercent: -50,
+        xPercent: -20,
+        autoAlpha: 1,
+        ease: Power4.easeInOut
+      })
+      .to('.ieo__img-coin--second', 0.3, {
+        yPercent: -50,
+        xPercent: 20,
+        autoAlpha: 1,
+        ease: Power4.easeInOut
+      });
+
+    const controller2 = new ScrollMagic.Controller();
+
+    const scene2 = new ScrollMagic.Scene({
+        triggerElement: '.ieo',
+        duration: 800,
+        triggerHook: 0.1
+      })
+      .setTween(tween2)
+      .setPin('.ieo')
+      .addTo(controller2);
+
+
+    // FAQ animate
+
+    const flightPath = {
+      curviness: 1.25,
+      autoRotate: true,
+      values: [{
+          x: 100,
+          y: -50
+        },
+        {
+          x: 150,
+          y: -200
+        },
+        {
+          x: 0,
+          y: -80
+        },
+        {
+          x: 0,
+          y: 0
+        },
+
+        {
+          x: window.innerWidth,
+          y: 0
+        }
+
+      ]
+    };
+
+    const flightPath2 = {
+      curviness: 1.25,
+      autoRotate: true,
+      values: [
+        {
+          x: 100,
+          y: -50
+        },
+        {
+          x: 150,
+          y: -200
+        },
+        {
+          x: 0,
+          y: -80
+        },
+        {
+          x: 100,
+          y: -50
+        },
+        {
+          x: 150,
+          y: -200
+        },
+        {
+          x: 0,
+          y: -80
+        },
+        {
+          x: 0,
+          y: 0
+        },
+
+        {
+          x: window.innerWidth,
+          y: 0
+        }
+
+      ]
+    };
+
+    const tween3 = new TimelineLite();
+
+    tween3.add(
+        TweenLite.to('.faq__img--plane-big', 1, {
+          bezier: flightPath,
+          ease: Power1.easeInOut,
+          autoAlpha: 1
+        }))
+      .add(
+        TweenLite.to('.faq__img--plane-small', 1.5, {
+          bezier: flightPath2,
+          ease: Power1.easeInOut,
+          autoAlpha: 1
+        }))
+
+    const controller3 = new ScrollMagic.Controller();
+
+    const scene3 = new ScrollMagic.Scene({
+        triggerElement: '.faq',
+        duration: 2000,
+        triggerHook: 0.1
+      })
+      .setTween(tween3)
+      .setPin('.faq')
+      .addTo(controller3);
+
+
+  }
+
+};
+
+widthAnimateInit(mobileWidth)
+mobileWidth.addListener(widthAnimateInit);
